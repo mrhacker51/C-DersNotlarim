456639dbc36e0285e579b07d474ee86487838137
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-08-12 00:47:43 +0300

    Create while.c

---
+++ while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	int sayi=0;
+	while(sayi<=100){
+
+		if(sayi%2 == 0){
+
+			printf("Sayilar=%d\n",sayi);
+
+		}
+
+		sayi++;
+	}	
+	return 0;
+}

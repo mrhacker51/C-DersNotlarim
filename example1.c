34d6e2889e76effdf41ac5994d6ea97b803d1898
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-09-16 05:32:23 +0300

    Create example1.c

---
+++ example1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	char kitapadi[10];
+
+	printf("Kitap Adını Giriniz : \n");
+	scanf("%s",kitapadi);
+
+	printf("Kitabın Adı : %s\n",kitapadi);
+	return 0;
+}

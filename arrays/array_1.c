188b3e3489aac9c7880429295588dbc88a62aaef
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-28 21:40:44 +0300

    Update array_1.c

--- arrays/array_1.c
+++ arrays/array_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+
+int main(){
+    int dizi[5];
+    dizi[0] = 5;
+    dizi[1] = 10;
+    dizi[2] = 15;
+    dizi[3] = 20;
+    dizi[4] = 25;
+    
+    
+    printf("Dizinin 4.İndex'indeki Sayi : %d",dizi[4]);
+}

4975a16e120c408dead6cb7c31edaa7a1e6b7612
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-28 21:52:06 +0300

    Create array_2.c

---
+++ arrays/array_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(){
+    
+    for(int i=0; i<=10; i++){
+        int dizi[5];
+        dizi[0] = 1;
+        dizi[1] = 2;
+        dizi[2] = 3;
+        dizi[3] = 4;
+        dizi[4] = 5;
+    
+        printf("Dizi : %d%d%d%d%d\n",dizi[0],dizi[1],dizi[2],dizi[3],dizi[4]);
+    }
+}

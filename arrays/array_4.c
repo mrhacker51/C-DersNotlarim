186530b1b548972046f37e09d9f78279db8d60dc
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-28 22:00:26 +0300

    Create array_4.c

---
+++ arrays/array_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(){
+    
+    int dizi[] = {1,2,3,4,5};
+    
+    printf("Dizi : \n%d\n%d\n%d\n%d\n%d",dizi[0],dizi[1],dizi[2],dizi[3],dizi[4]);
+}
